1867b400e3158a0c0e25db25ac53e002926fad57
Edzel <dev.edzeldacanay@gmail.com>
2023-07-14 13:31:38 +0800

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+
+const Home = () => {
+
+  const {products} = useContext(ProductContext);
+  const filteredProduct = products.filter(item => {
+    return item.category === "men's clothing" || item.category === "women's clothing"
+  });
+
+  return <div>
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+          {filteredProduct.map(product => {
+            return <div className='w-full h-[300px] bg-blue-200' key={product.title}>{product.title}</div>
+          })}
+      </div>
+      </div>
+    </section>
+  </div>;
+};
+
+export default Home;
